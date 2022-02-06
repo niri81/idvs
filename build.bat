0d782cfbf9a1a811872f4e01f398a6ac8222d071
niri81 <nickrieser18@gmail.com>
2022-02-06 20:51:02 +0100

    Added build file

---
+++ build.bat
@@ -0,0 +1,2 @@
+pip install -r "requirements.txt"
+pyinstaller -F -n "IDVS" -i "./pictures/logo.ico" -w "main.py"
---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pip install -r "requirements.txt"
+pyinstaller -F -n "IDVS" -i "./pictures/logo.ico" -w "main.py"
